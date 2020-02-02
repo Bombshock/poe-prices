c07e053f9c8c9a74663c03337198dedda37ca605
Bombshock <havak88@gmail.com>
2020-02-03 00:40:35 +0100

    better item filter

--- src/app/pipes/item-filter.pipe.ts
+++ src/app/pipes/item-filter.pipe.ts
@@ -1,19 +1,33 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { Item } from '../models/item';
 
-@Pipe( {
+const typeMapping = {
+  1: 'normal',
+  2: 'magic',
+  3: 'rare',
+  4: 'unique',
+  5: 'currency',
+  6: 'divination card'
+}
+
+@Pipe({
   name: 'itemFilter'
-} )
+})
 export class ItemFilterPipe implements PipeTransform {
 
-  transform( items: Item[], filter: string ): any {
+  transform(items: Item[], filter: string): any {
     const _filter = filter.trim().toLocaleLowerCase();
-    if ( !filter ) {
+    if (!filter) {
       return items;
     }
-    return items.filter( item => {
-      return item.name.toLocaleLowerCase().indexOf( _filter ) !== -1 || item.typeLine.toLocaleLowerCase().indexOf( filter ) !== -1
-    } );
+    return items.filter(item => {
+      return (
+        item.name.toLocaleLowerCase().indexOf(_filter) !== -1 ||
+        item.typeLine.toLocaleLowerCase().indexOf(_filter) !== -1 ||
+        `${item.frameType}` === _filter ||
+        `${typeMapping[item.frameType]}`.indexOf(_filter) !== -1
+      )
+    });
   }
 
 }
