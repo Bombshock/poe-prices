d1f31346ac4642d8b439aaffa3f6dcc8af9f97e8
Alexander Bau <alexander.bau@nfon.com>
2020-02-04 14:53:08 +0100

    added menu

--- src/app/components/title-bar/title-bar.component.ts
+++ src/app/components/title-bar/title-bar.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { TitleService } from 'src/app/services/title.service';
+import { ApiService } from 'src/app/services/api.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { AuthService } from 'src/app/services/auth.service';
 
 const remote = window[ 'require' ]( 'electron' ).remote;
 
@@ -11,7 +14,11 @@ const remote = window[ 'require' ]( 'electron' ).remote;
 export class TitleBarComponent implements OnInit {
 
   constructor(
-    public titleService: TitleService
+    public titleService: TitleService,
+    public auth: AuthService,
+
+    private api: ApiService,
+    private snackBar: MatSnackBar
   ) { }
 
   ngOnInit() {
@@ -34,5 +41,30 @@ export class TitleBarComponent implements OnInit {
   close() {
     const window = remote.getCurrentWindow();
     window.close();
+  }
+
+  clearCache() {
+    this.api.clearCache();
+    this.snackBar.open( 'chache cleared', null, {
+      duration: 2000,
+    } );
+  }
+
+  @HostListener( 'window:keyup', [ '$event' ] )
+  keyEvent( event: KeyboardEvent ) {
+    console.log( event );
+
+    if ( event.key === 'F12' ) {
+      this.openDevTools();
+    }
+  }
+
+  openDevTools() {
+    window[ 'require' ]( 'electron' ).remote.BrowserWindow.getAllWindows()[ 0 ].openDevTools();
+  }
+
+  logout() {
+    this.auth.logout();
+    this.api.clearCache();
   }
 }
