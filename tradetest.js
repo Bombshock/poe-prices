f8dbcf7cd54596a5e3efe8db5af8c93e3496cdf4
Alexander Bau <alexander.bau@nfon.com>
2020-02-07 17:03:39 +0100

    angular 9

--- tradetest.js
+++ tradetest.js
@@ -1,10 +1,15 @@
 const categorys = require( './src/assets/tradecategorys.json' );
 const items = require( './src/assets/tradeitems.json' );
 const { clipboard } = require( 'electron' );
+const cats = categorys.map( cat => cat.text.toLocaleLowerCase() );
 
 let exit = false;
 
+// console.log( match( 'ashscale talisman' ) );
+// process.exit( 0 );
+
 items.forEach( grp => {
+    console.log( '' );
     console.log( '=============', grp.label, '=============' );
     grp.entries
         .filter( entry => {
@@ -12,27 +17,15 @@ items.forEach( grp => {
         } )
         .forEach( entry => {
             const type = entry.type.toLocaleLowerCase();
-            matches = categorys.filter( cat => {
-
-                if ( cat.not && checkList( type, cat.not ) ) {
-                    return false;
-                }
-
-                if ( type.indexOf( cat.text.toLocaleLowerCase() ) !== -1 ) {
-                    return true;
-                }
-
-                if ( cat.tags ) {
-                    return checkList( type, cat.tags );
-                }
-
-                return false;
-            } );
+            matches = match( type );
             if ( matches.length !== 1 ) {
                 if ( !exit ) {
                     clipboard.writeText( type, 'selection' )
                 }
-                console.log( entry.text );
+                console.log( type );
+                if ( matches.length > 1 ) {
+                    console.log( matches )
+                }
                 exit = true;
                 process.exit( 0 );
             }
@@ -42,6 +35,40 @@ items.forEach( grp => {
         process.exit( 0 );
     }
 } );
+
+function match( type ) {
+    const tagResults = categorys.filter( cat => {
+        if ( cat.tags && cat.tags.includes( type ) ) {
+            return true;
+        }
+    } );
+
+    if ( tagResults.length ) {
+        return tagResults;
+    }
+
+    return categorys.filter( cat => {
+        const catText = cat.text.toLocaleLowerCase();
+
+        if ( checkList( type, cats.filter( c => c !== catText && c !== 'ring' ) ) ) {
+            return false;
+        }
+
+        if ( cat.not && checkList( type, cat.not ) ) {
+            return false;
+        }
+
+        if ( type.indexOf( catText ) !== -1 ) {
+            return true;
+        }
+
+        if ( cat.tags ) {
+            return checkList( type, cat.tags );
+        }
+
+        return false;
+    } );
+}
 
 function checkList( search, list ) {
     for ( let i = 0; i < list.length; i++ ) {
